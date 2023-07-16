eb5696c53ee6576af7d6b1719f76c2c95018c298
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-07-16 20:52:25 +0100

    Create a folder named models with an empty file __init__.py inside
    the base.py will be the base of allthe other classes

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module name: base, Class: Base
+"""
+
+
+import json
+import turtle
+
+
+class Base:
+    """This is the base of all the other classes
+        method: __init__()"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
