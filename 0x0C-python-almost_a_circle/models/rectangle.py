7d4901ee36dd4ae3b17cecc04f067d85ef9587c3
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-07-17 13:42:00 +0100

    "Improve attribute assignment in Rectangle class update method"
    This commit enhances the `update` method in the `Rectangle` class to provide more flexible attribute assignment. The method now supports both positional arguments (`*args`) and keyword arguments (`**kwargs`), allowing for easier and more intuitive updates to the `Rectangle` instance attributes. This improvement simplifies the attribute assignment process and enhances the overall usability of the `update` method.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,16 +95,22 @@ class Rectangle(Base):
             f"{self.__width}/{self.__height}"
         )
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Assigns arguments to each attribute"""
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        if args:
+            num_args = len(args)
+            if num_args > 0:
+                self.id = args[0]
+            if num_args > 1:
+                self.width = args[1]
+            if num_args > 2:
+                self.height = args[2]
+            if num_args > 3:
+                self.x = args[3]
+            if num_args > 4:
+                self.y = args[4]
+        else:
+            """to assign values based on the kwargs"""
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
