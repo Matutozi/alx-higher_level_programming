74437848cc06a64bcadc1b4030a2a0b479cd5989
Sobowale Emmanuel <sobowalegz2@gmail.com>
2023-09-11 18:58:24 +0100

    commnd line argument check

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Checks command line for the number of arguments
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else if (process.argv.length > 3) {
+  console.log('Arguments found');
+}
